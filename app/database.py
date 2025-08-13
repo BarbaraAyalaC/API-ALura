4b62bed7513254e6fcaf5f5f552c5fbfc422d1fa
BarbaraAyalaC <barbaraayalacastillo@gmail.com>
2025-08-13 17:33:00 -0400

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Usamos SQLite para simplificar
+
+# Configuración de la base de datos
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
